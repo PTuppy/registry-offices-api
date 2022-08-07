507705d79aafd03c11eec0555c90d2546f9601e4
PriscilaTuppy <priscila.tuppy@amedigital.com>
2022-08-07 13:41:04 -0300

    Template inicial spring boot

---
+++ src/main/java/com/ptuppy/registryofficesapi/RegistryOfficesApiApplication.java
@@ -0,0 +1,13 @@
+package com.ptuppy.registryofficesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryOfficesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryOfficesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptuppy/registryofficesapi/RegistryOfficesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptuppy.registryofficesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryOfficesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
